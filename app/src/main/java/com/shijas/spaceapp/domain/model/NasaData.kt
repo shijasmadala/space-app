50069fc1112ce6de1d9bc88855689b2dd97fb8e1
shijasmadala <shijasmadala9446@gmail.com>
2022-08-13 20:24:21 +0530

    Add domain layer packages and model class nasa data

---
+++ app/src/main/java/com/shijas/spaceapp/domain/model/NasaData.kt
@@ -0,0 +1,9 @@
+package com.shijas.spaceapp.domain.model
+
+data class NasaData(
+    val date: String,
+    val explanation: String,
+    val hduUrl: String,
+    val title: String,
+    val url: String
+)
