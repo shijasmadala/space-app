135eea4f93c291da8efa320446c4c79336a7e969
shijasmadala <shijasmadala9446@gmail.com>
2022-08-13 20:18:33 +0530

    Add app module and repository module

---
+++ app/src/main/java/com/shijas/spaceapp/app/di/AppModule.kt
@@ -0,0 +1,17 @@
+package com.shijas.spaceapp.app.di
+
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi() = Moshi.Builder().build()
+}
---
+++ app/src/main/java/com/shijas/spaceapp/app/di/RepositoryModule.kt
@@ -0,0 +1,10 @@
+package com.shijas.spaceapp.app.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class RepositoryModule {
+}
