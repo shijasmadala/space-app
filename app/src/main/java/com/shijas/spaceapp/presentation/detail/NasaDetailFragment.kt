0fd917424df67d9f7469b077644d73cd70e5002c
shijasmadala <shijasmadala9446@gmail.com>
2022-08-14 10:24:05 +0530

    Create nasa detail fragment for display the image and more details of nasa data

---
+++ app/src/main/java/com/shijas/spaceapp/presentation/detail/NasaDetailFragment.kt
@@ -0,0 +1,14 @@
+package com.shijas.spaceapp.presentation.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.shijas.spaceapp.R
+
+
+
+class NasaDetailFragment : Fragment(R.layout.fragment_nasa_detail) {
+
+}
