69527647eef1a763dc440009f03edf926ce5946c
shijasmadala <shijasmadala9446@gmail.com>
2022-08-14 10:06:40 +0530

    Add domain layers model and repository class

--- app/src/main/java/com/shijas/spaceapp/app/di/RepositoryModule.kt
+++ app/src/main/java/com/shijas/spaceapp/app/di/RepositoryModule.kt
@@ -1,10 +1,17 @@
 package com.shijas.spaceapp.app.di
 
+import com.shijas.spaceapp.data.repository.HomeRepositoryImpl
+import com.shijas.spaceapp.domain.repository.HomeRepository
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 
 @Module
 @InstallIn(ViewModelComponent::class)
-class RepositoryModule {
+abstract class RepositoryModule {
+    @Binds
+    abstract fun bindHomeNasaDataRepository(homeRepositoryImpl: HomeRepositoryImpl) : HomeRepository
+
+
 }
