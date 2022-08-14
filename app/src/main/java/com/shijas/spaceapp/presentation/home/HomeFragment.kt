f437954e65b2bd6c8f2cde0bfda89945aaaa9fd3
shijasmadala <shijasmadala9446@gmail.com>
2022-08-14 10:13:59 +0530

    Create home fragment for display nasa data

---
+++ app/src/main/java/com/shijas/spaceapp/presentation/home/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.shijas.spaceapp.presentation.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.shijas.spaceapp.R
+
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
