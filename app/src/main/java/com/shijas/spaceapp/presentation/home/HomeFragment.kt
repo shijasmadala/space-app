2354da1df734424868e792dcf934deb409fcce5f
shijasmadala <shijasmadala9446@gmail.com>
2022-08-14 10:19:59 +0530

    Connect adapter class and fragment and set item data in adapter

--- app/src/main/java/com/shijas/spaceapp/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/shijas/spaceapp/presentation/home/HomeFragment.kt
@@ -5,9 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.shijas.spaceapp.R
+import com.shijas.spaceapp.databinding.FragmentHomeBinding
+import com.shijas.spaceapp.presentation.main.MainActivityViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
+class HomeFragment : Fragment(R.layout.fragment_home),NasaItemAdapter.ClickListener {
+    private lateinit var binding: FragmentHomeBinding
+    private val mainViewModel by activityViewModels<MainActivityViewModel>()
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+    private val nasaItemAdapter by lazy {
+        NasaItemAdapter(this).apply {
+            submitList(mainViewModel.nasaData)
+        }
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentHomeBinding.bind(view)
+
+        binding.recyclerNasaData.adapter = nasaItemAdapter
+    }
+
+    override fun onClick(position: Int) {
+//        mainViewModel.selectedIndex = position
+//        findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToNasaDetailFragment())
+    }
 }
